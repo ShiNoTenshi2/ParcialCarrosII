a5affa8693bba6626ca706ad6fe1674bf397b2e3
ESTUDIANTE <ESTUDIANTE@SI5-19>
2025-05-08 09:27:19 -0500

    ?

--- concesionarioCarroFX/src/controller/ConcesionarioController.java
+++ concesionarioCarroFX/src/controller/ConcesionarioController.java
@@ -1,46 +1,97 @@
 package controller;
 
+import java.sql.Connection;
+
+import application.Main;
+import data.CarroDAO;
+import data.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Carro;
 
 public class ConcesionarioController {
 
     @FXML
-    private TableColumn<?, ?> columnCantidadD;
+    private TableColumn<Carro, Integer> columnCantidadD;
 
     @FXML
-    private TableColumn<?, ?> columnColorE;
+    private TableColumn<Carro, String> columnColorE;
 
     @FXML
-    private TableColumn<?, ?> columnMarcaD;
+    private TableColumn<Carro, String> columnMarcaD;
 
     @FXML
-    private TableColumn<?, ?> columnMarcaE;
+    private TableColumn<Carro, String> columnMarcaE;
 
     @FXML
-    private TableColumn<?, ?> columnModeloD;
+    private TableColumn<Carro, String> columnModeloD;
 
     @FXML
-    private TableColumn<?, ?> columnModeloE;
+    private TableColumn<Carro, String> columnModeloE;
 
     @FXML
-    private TableView<?> tableCarrosDisponibles;
+    private TableView<Carro> tableCarrosDisponibles;
 
     @FXML
-    private TableView<?> tableCarrosEliminados;
- 
+    private TableView<Carro> tableCarrosEliminados;
     
+    private Connection connection = DBConnection.getInstance().getConnection();
+    private CarroDAO carroDAO = new CarroDAO(connection);
+    
+    @FXML
+    public void initialize() {
+        ObservableList<Carro> availableCarros = FXCollections.observableArrayList();
+        
+        for (Carro carro: carroDAO.fetchDisponible()) {
+            availableCarros.add(carro); 
+        }
 
+        ObservableList<Carro> noavailableCarros = FXCollections.observableArrayList();
+        
+        for (Carro carro: carroDAO.fetchEliminados()) {
+            noavailableCarros.add(carro); 
+        }
+        
+        columnCantidadD.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
+        columnColorE.setCellValueFactory(new PropertyValueFactory<>("color"));
+        columnMarcaD.setCellValueFactory(new PropertyValueFactory<>("marca"));
+        columnMarcaE.setCellValueFactory(new PropertyValueFactory<>("marca"));
+        columnModeloD.setCellValueFactory(new PropertyValueFactory<>("modelo"));
+        columnModeloE.setCellValueFactory(new PropertyValueFactory<>("modelo"));
+
+        // Asigna los datos a la TableView
+        tableCarrosDisponibles.setItems(availableCarros);
+        tableCarrosEliminados.setItems(noavailableCarros);
+    }
+
+    
     @FXML
     void eliminar(ActionEvent event) {
-
+        if (!tableCarrosDisponibles.getSelectionModel().isEmpty()) {
+            Carro carro = tableCarrosDisponibles.getSelectionModel().getSelectedItem();
+            carroDAO.eliminar(carro.getReferencia()); 
+            initialize();
+        } else {
+            Main.showAlert("Seleccione un modelo", "Seleccione un modelo", "Debe seleccionar un dato de la tabla", Alert.AlertType.WARNING);
+        }
     }
 
     @FXML
     void recuperar(ActionEvent event) {
-
+        if (!tableCarrosEliminados.getSelectionModel().isEmpty()) {
+            Carro carro = tableCarrosEliminados.getSelectionModel().getSelectedItem();
+            carroDAO.recuperar(carro.getReferencia()); 
+            initialize();
+        } else {
+            Main.showAlert("Seleccione un modelo", "Seleccione un modelo", "Debe seleccionar un dato de la tabla", Alert.AlertType.WARNING);
+        }
     }
-
 }
+
+
--- concesionarioCarroFX/src/data/CarroDAO.java
+++ concesionarioCarroFX/src/data/CarroDAO.java
@@ -17,28 +17,69 @@ public class CarroDAO {
         this.connection = connection;
     }
 
+    public ArrayList<Carro> fetchDisponible() {
+        ArrayList<Carro> carros = new ArrayList<>(); 
+        String sql = "SELECT * FROM Carro WHERE is_deleted=0";  
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) { 
+            while (rs.next()) {  
+                int referencia = rs.getInt("referencia");  
+                String marca = rs.getString("marca");   
+                String modelo = rs.getString("modelo");
+                String color = rs.getString("color");   
+                int cantidad = rs.getInt("cantidad");     
 
-
-	public ArrayList<Carro> fetchDisponibles() {
-        ArrayList<Carro> carros = new ArrayList<>();
-
-        return carros;
-	}
+                Carro carro = new Carro(referencia, marca, modelo, color, cantidad);
+                carros.add(carro); 
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        }
+        return carros; 
+    }
 
 	public ArrayList<Carro> fetchEliminados() {
         ArrayList<Carro> carros = new ArrayList<>();              
-        
+        String sql = "SELECT * FROM Carro WHERE is_deleted=1";  
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) { 
+            while (rs.next()) {  
+                int referencia = rs.getInt("referencia");  
+                String marca = rs.getString("marca");   
+                String modelo = rs.getString("modelo");   
+                String color = rs.getString("color");   
+                int cantidad = rs.getInt("cantidad");     
+
+                Carro carro = new Carro(referencia, marca, modelo, color, cantidad);
+                carros.add(carro); 
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        }
+
         return carros;
 	}
 
-
 	public void eliminar(int referencia) {
+        String sql = "UPDATE Carro SET is_deleted=1 WHERE referencia=?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, referencia);  
+            stmt.executeUpdate(); 
+        } catch (SQLException e) {
+            e.printStackTrace();  
+        }
+    }
 
-	}
 
 	public void recuperar(int referencia) {
-
-	}
+        String sql = "UPDATE Carro SET is_deleted=0 WHERE referencia=?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, referencia);  
+            stmt.executeUpdate(); 
+        } catch (SQLException e) {
+            e.printStackTrace();  
+        }
+    }
 	
 	
 	
